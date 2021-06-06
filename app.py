0a371930b42b023b4757d233b2290ddc5ee1cd9c
nikof7 <wjuice7@gmail.com>
2021-06-06 17:30:29 -0300

    improivng

--- app.py
+++ app.py
@@ -6,28 +6,21 @@ base_url = "https://www.ecoregistros.org"
 
 sp = 'Alectoris-chukar'
 
-def buscar_n_registros(name):
+def n_registros(name):
+
+	# Busca la cantidad de registros que hay para dicho taxa (name)
+	# Se podría agregar la cantidad de registros que hay por país
+		# URU - 520
+		# ARG - 201
+		# BRA - 102
+
 	url = base_url + '/ficha/' + name
 	r = requests.get(url)
 	soup = BeautifulSoup(r.content,'html.parser')
 	n_registros = str(soup.find_all('span', class_='tamanioPequenio colorFuenteDescripcion center')[4])
 	return n_registros[86:-11]
 
-def abrir_img_registro(id):
-	url_by_id = base_url + "/site/imagen.php?id=" + id
-	r = requests.get(url_by_id)
-	soup = BeautifulSoup(r.content,'html.parser')
-
-	# Busca en el html 'img'
-	imagen = soup.find_all('img')
-	#imagen[1] corresponde al lugar donde siempre se encuentra el src de la imagen a buscar
-	img_src = str(imagen[1]).split('"')
-	# crea el link con la base_url y
-	link_imagen = base_url + img_src[5][2:]
-
-	return webbrowser.open(link_imagen, new=1)
-
-def id_ultima_img(taxa):
+def imagen():
 	url = base_url + '/ficha/' + taxa
 
 	r = requests.get(url)
@@ -39,9 +32,18 @@ def id_ultima_img(taxa):
 
 	forma_lista = div_con_datos.text.split(' ')
 
-	id_final = forma_lista[2].split("\xa0")[0]
+	id = forma_lista[2].split("\xa0")[0]
 
-	return(id)
+	url_by_id = base_url + "/site/imagen.php?id=" + id
 
+	r2 = requests.get(url_by_id)
+	soup2 = BeautifulSoup(r.content,'html.parser')
 
-print(id_ultima_img('Alectoris-chukar')
+	# Busca en el html 'img'
+	imagen = soup.find_all('img')
+	#imagen[1] corresponde al lugar donde siempre se encuentra el src de la imagen a buscar
+	img_src = str(imagen[1]).split('"')
+	# crea el link con la base_url y
+	link_imagen = base_url + img_src[5][2:]
+
+	return webbrowser.open(link_imagen, new=1)
